3061db0d273c36ff11fac02e52e14c1279558b8c
spayne92 <sfpr43@mst.edu>
2021-10-20 04:28:39 -0500

    JWT Authorization
    Basic token generation and validation.
    
    Incorporates token into session state.

---
+++ Infrastructure/JwtModels/JwtTokenConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BaseCoreAPI.Infrastructure
+{
+    public class JwtTokenConfig
+    {
+        [JsonPropertyName("key")]
+        public string Key { get; set; }
+
+        [JsonPropertyName("issuer")]
+        public string Issuer { get; set; }
+
+        [JsonPropertyName("audience")]
+        public string Audience { get; set; }
+
+        [JsonPropertyName("accessTokenExpiration")]
+        public int AccessTokenExpiration { get; set; }
+
+        [JsonPropertyName("refreshTokenExpiration")]
+        public int RefreshTokenExpiration { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,16 @@
-using BaseCoreAPI.Data;
+using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
+using Microsoft.IdentityModel.Tokens;
+using BaseCoreAPI.Data;
+using BaseCoreAPI.Infrastructure;
+using BaseCoreAPI.Services;
 
 namespace BaseCoreAPI
 {
@@ -21,6 +27,31 @@ namespace BaseCoreAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var jwtTokenConfig = _config.GetSection("Tokens").Get<JwtTokenConfig>();
+            services.AddSingleton(jwtTokenConfig);
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtTokenConfig.Issuer,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtTokenConfig.Key)),
+                    ValidAudience = jwtTokenConfig.Audience,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ClockSkew = jwtTokenConfig.AccessTokenExpiration > 0 ? TimeSpan.FromMinutes(jwtTokenConfig.AccessTokenExpiration) : TimeSpan.FromMinutes(1)
+                };
+            });
+
             services.AddDbContext<BaseContext>(cfg =>
             {
                 cfg.UseMySql(_config.GetConnectionString("BaseConnectionString"), new MySqlServerVersion(new Version(8, 0, 25)));
@@ -28,6 +59,8 @@ namespace BaseCoreAPI
 
             // DependencyInjection registration.
             services.AddTransient<BaseSeeder>();
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<ITokenService, TokenService>();
 
             services.AddControllers();
         }
@@ -35,8 +68,21 @@ namespace BaseCoreAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseRouting();
+            app.UseSession();
+            app.Use(async (context, next) =>
+            {
+                var token = context.Session.GetString("Token");
+                if (!string.IsNullOrEmpty(token))
+                {
+                    // Adds token to HTTP header if it exists in the session.
+                    context.Request.Headers.Add("Authorization", "Bearer " + token);
+                }
+                await next();
+            });
 
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(cfg =>
             {
                 // Simple routing for API controllers. 
